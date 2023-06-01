2214b184652d7bd46cc96961272020b81c514b54
AfroGrit <jlkitunka@gmail.com>
2023-06-01 21:15:46 +0200

    feat: tracking with autologs

--- wk2/hw/train.py
+++ wk2/hw/train.py
@@ -5,6 +5,11 @@ import click
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
+mlflow.set_tracking_uri("sqlite:///duration_mlflow.db")
+mlflow.set_experiment("tracking-hw-2")
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -17,17 +22,23 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
+
+
 def run_train(data_path: str):
+    
+    mlflow.sklearn.autolog()
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
+        
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 if __name__ == '__main__':
     run_train()
