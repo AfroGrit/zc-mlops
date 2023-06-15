b8b56b2af1c00dc8acefdaffa942a3f7055c7cf3
AfroGrit <jlkitunka@gmail.com>
2023-06-15 11:33:26 +0200

    feat: predict using requests to flask

--- wk4/test.py
+++ wk4/test.py
@@ -1,12 +1,13 @@
-import predict
 
-ride = {
+import requests
+
+payload = {
     "PULocationID": 10,
     "DOLocationID": 50,
     "trip_distance": 40
 }
 
-feats = predict.prepare_features(ride)
-prediction = predict.predict(feats)
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=payload)
 
-print(prediction)
+print(response.json())
